1121ac8c73c92280913b72ee2267e118abcb84ed
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2025-01-12 23:54:56 +0000

    Initial commit

---
+++ assets/js/form.js
@@ -0,0 +1,22 @@
+function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission
+    const form = document.getElementById('rsvp_form');
+    const formData = new FormData(form);
+
+    // Replace with your Google Form's URL
+    const formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSdlO9_i-ItB_Oken-4ZPiKvcDEJGHgaZsvu2LG4XahcpweFEQ/formResponse';
+
+    fetch(formUrl, {
+        method: 'POST',
+        mode: 'no-cors',
+        body: formData
+    })
+        .then(response => {
+            alert('Thank you for your RSVP!');
+            form.reset();
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+            alert('Oops! Something went wrong. Please try again later.');
+        });
+}
