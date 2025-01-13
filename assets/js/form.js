b29381306693307e970771f244afa33b13734d52
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2025-01-13 00:34:47 +0000

    Experimental

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,6 +6,8 @@ function handleFormSubmit(event) {
     // Replace with your Google Form's URL
     const formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSdlO9_i-ItB_Oken-4ZPiKvcDEJGHgaZsvu2LG4XahcpweFEQ/formResponse';
 
+    alert("This website is still experimental. Come back when it's ready.")
+    return
     fetch(formUrl, {
         method: 'POST',
         mode: 'no-cors',
